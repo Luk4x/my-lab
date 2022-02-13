ebc116e457f947bc1281c100e14f84394001cb5a
Luk4x <luk4xm4ci3l@gmail.com>
2022-02-13 11:47:57 -0300

    js lesson 5

---
+++ Javascript/variables.js
@@ -0,0 +1,15 @@
+var number1 = 1 // não recomendado
+let number2 = 2
+const number3 = 3
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
+
+number1 = 1+1
+number2 = 2+2
+// number3 é uma constante
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
