3fbffcc7666d0d224d0e9aadb5bb306b1680b9da
Luk4x <luk4xm4ci3l@gmail.com>
2022-03-10 05:46:44 -0300

    lesson13

--- javascript/javascriptWeb/practices.js
+++ javascript/javascriptWeb/practices.js
@@ -1,15 +1,100 @@
-// getting attributes from elements:
-const input1 = document.getElementsByName('input1');
-console.log(input1[0].attributes); // .attributes to see all
-console.log(input1[0].value); // specify in lists
-const title4 = document.querySelector('h1');
-console.log(title4.textContent);
+// about getElementById() method:
+const title = document.getElementById('title'); // classic
+console.log(title); // returns element
 console.log('');
 
-// manipulating elements
-title4.innerText = 'Anime List:';
-input1[0].value = 'Comedy';
-const paragraphs4 = document.querySelectorAll('p');
-paragraphs4[0].textContent = 'Blend S';
-paragraphs4[1].innerHTML = '<abbr title="that one is really good">Seitokai Yakuindomo</abbr>';
-paragraphs4[2].innerText = 'Non Non Biyori';
+// about getElementsByClassName() method:
+const paragraphs = document.getElementsByClassName('paragraph'); // returns a HTML Collection, who looks like a array
+console.log(paragraphs);
+// paragraphs.forEach(element => {   -> HTML Collection is limited.
+//     console.log(element);
+// });
+console.log(paragraphs[0]);
+console.log(paragraphs[1]);
+console.log('');
+
+// about getElementsByTagName() method:
+const allParagraphs = document.getElementsByTagName('p');
+console.log(allParagraphs);
+
+const body = document.getElementsByTagName('body'); // obligatory returns a HTML collection
+console.log(body);
+console.log('');
+
+// about querySelector() method:
+const paragraph1 = document.querySelector('.paragraph'); // returns the first one found
+console.log(paragraph1);
+
+const body2 = document.querySelector('body'); // returns element
+console.log(body2);
+
+const title2 = document.querySelector('#title');
+console.log(title2);
+console.log('');
+
+// about querySelectorAll() method:
+const paragraphs2 = document.querySelectorAll('.paragraph');
+console.log(paragraphs2);
+paragraphs2.forEach(element => {
+    // NodeList isn't limited like HTML Collection.
+    console.log(element);
+});
+
+const allParagraphs2 = document.querySelectorAll('p');
+console.log(allParagraphs2);
+
+const title3 = document.querySelectorAll('#title'); // obligatory returns a NodeList
+console.log(title3);
+console.log('');
+
+// about getElementsByName() method:
+const input = document.getElementsByName('input1'); // obligatory returns a NodeList
+console.log(input);
+
+const allParagraphs3 = document.getElementsByName('paragraph');
+console.log(allParagraphs3);
+console.log('');
+
+// manipulating html elements e styles with DOM:
+const main = document.getElementsByTagName('main');
+main[0].style.textAlign = 'center';
+body[0].style.backgroundColor = 'rgb(50, 50, 50)';
+body2.style.color = 'gray';
+
+title.innerText = 'Animes:';
+title.style.color = 'lightGray';
+
+input[0].value = 'Comedy';
+input[0].style.margin = '0 10px 15px 0';
+input[0].style.color = 'red';
+input[0].disabled = true;
+
+allParagraphs2[0].textContent = 'Blend S';
+allParagraphs2[1].innerHTML = '<abbr title="that one is really good">Seitokai Yakuindomo</abbr>'; // accept html tags inside
+allParagraphs2[2].innerText = 'Non Non Biyori';
+
+// getting attributes from elements:
+console.log(input[0].attributes); // .attributes to see all list
+console.log(input[0].value); // specify in lists
+console.log(title.textContent);
+console.log('');
+
+// getting styles from elements:
+console.log(input[0].style);
+console.log(input[0].style.margin);
+console.log(title.style.color);
+console.log('');
+
+// events: (list: https://www.w3schools.com/tags/ref_eventattributes.asp)
+const changeTitleColor = () => {
+    title.style.color = 'red';
+};
+
+// set default title color
+// calling function via JS (directly)
+title.onmouseout = eventParameters => {
+    // every event has a list of parameters
+    console.log(eventParameters);
+    console.log(eventParameters.target);
+    title.style.color = 'lightGray';
+};
