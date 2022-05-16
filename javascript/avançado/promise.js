8e25397d0f28197372ac40aaec052ff41ed4ce59
Luk4x <luk4xm4ci3l@gmail.com>
2022-05-16 16:05:24 -0300

    promises p2

--- javascript/avançado/promise.js
+++ javascript/avançado/promise.js
@@ -1,3 +1,29 @@
+// creating a promise
+const myPromise = new Promise((resolve, reject) => {
+    const pNum = 12;
+    if (pNum == 11) resolve(pNum);
+    else reject(pNum);
+});
+
+// consuming my owm promise
+myPromise
+    .then(pValue => {
+        console.log(`${pValue} is right.`);
+        return pValue - 1;
+    })
+    .then(tValue => {
+        console.log(`but ${tValue} is better.`);
+    })
+    .catch(pValue => {
+        console.log(`${pValue} is wrong.`);
+    })
+    .finally(() => {
+        console.log('end.');
+    });
+
+// consuming a external promise (a API that return a promise)
+
+/*
 const myPromise = new Promise((resolve, reject) => {
     const name = 'Luk4x';
 
@@ -19,3 +45,4 @@ myPromise
         // reject return
         console.log(promiseReturn);
     });
+*/
