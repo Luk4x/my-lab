d0bede08572eabaee4ee52b72ebfb7b2b6e17526
Luk4x <luk4xm4ci3l@gmail.com>
2022-05-19 06:48:41 -0300

    async/await p2

--- javascript/avançado/promises/asyncAwait.js
+++ javascript/avançado/promises/asyncAwait.js
@@ -1,12 +1,50 @@
-const url = 'https://jsonplaceholder.typicode.com/users';
+/*
+    const url = 'https://jsonplaceholder.typicode.com/users';
 
-const getUsers = () => {
-    // fetch(url).then(usersData => {
-    //     console.log(usersData);
-    // });
+    const getUsers = () => {
+        // fetch(url).then(usersData => {
+        //     console.log(usersData);
+        // });
 
-    const usersData = fetch(url);
-    console.log(usersData);
+        const usersData = fetch(url);
+        console.log(usersData);
+    };
+
+    getUsers();
+*/
+
+/*
+    const sayHi = () => {
+        return 'hi';
+    };
+    console.log(sayHi());
+
+    console.log(); // --------------------------------------------
+
+    const aSayHi = async () => {
+        return 'hi';
+    };
+    console.log(aSayHi());
+    aSayHi().then(sayHiReturn => console.log(sayHiReturn));
+*/
+
+// const sum = (n1, n2) => {
+//     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+//         throw 'Enter only numbers';
+//     }
+
+//     return n1 + n2;
+// };
+// console.log(sum(1, 'a'));
+
+const aSum = async (n1, n2) => {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw 'Enter only numbers';
+    }
+
+    return n1 + n2;
 };
-
-getUsers();
+console.log(aSum(1, 'a'));
+aSum(1, 'a')
+    .then(aSumResolveReturn => console.log(aSumResolveReturn))
+    .catch(aSumRejectReturn => console.log('ErrOr:', aSumRejectReturn));
