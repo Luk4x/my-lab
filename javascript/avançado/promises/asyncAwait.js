c24562a348c65767719615cbbbd71533b1b4686d
Luk4x <luk4xm4ci3l@gmail.com>
2022-05-21 11:17:48 -0300

    async/await p3

--- javascript/avançado/promises/asyncAwait.js
+++ javascript/avançado/promises/asyncAwait.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 /*
     const url = 'https://jsonplaceholder.typicode.com/users';
 
@@ -28,23 +30,66 @@
     aSayHi().then(sayHiReturn => console.log(sayHiReturn));
 */
 
-// const sum = (n1, n2) => {
-//     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-//         throw 'Enter only numbers';
-//     }
+/*
+    // const sum = (n1, n2) => {
+    //     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //         throw 'Enter only numbers';
+    //     }
 
-//     return n1 + n2;
+    //     return n1 + n2;
+    // };
+    // console.log(sum(1, 'a'));
+
+    const aSum = async (n1, n2) => {
+        if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+            throw 'Enter only numbers';
+        }
+
+        return n1 + n2;
+    };
+    console.log(aSum(1, 'a'));
+    aSum(1, 'a')
+        .then(aSumResolveReturn => console.log(aSumResolveReturn))
+        .catch(aSumRejectReturn => console.log('ErrOr:', aSumRejectReturn));
+*/
+
+/*
+    const url = 'https://jsonplaceholder.typicode.com/users';
+
+    const getUsers = async () => {
+        try {
+            const usersData = await fetch(url);
+            console.log(usersData);
+        } catch (err) {
+            console.log('ErrOr: ', err);
+        }
+
+        // const usersData = await fetch(url);
+        // return usersData;
+    };
+    getUsers(); // .then(res => console.log(res)).catch(err => console.log('ErrOr: ', err));
+*/
+
+// const get3Pokemons = async (n1, n2, n3) => {
+//     const pokemon1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${n1}`).then(res => res.json());
+//     const pokemon2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${n2}`).then(res => res.json());
+//     const pokemon3 = await fetch(`https://pokeapi.co/api/v2/pokemon/${n3}`).then(res => res.json());
+
+//     console.log(pokemon1.name);
+//     console.log(pokemon2.name);
+//     console.log(pokemon3.name);
 // };
-// console.log(sum(1, 'a'));
+// get3Pokemons(2, 5, 9);
 
-const aSum = async (n1, n2) => {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw 'Enter only numbers';
-    }
+const getPokemon = pokemonNum => `https://pokeapi.co/api/v2/pokemon/${pokemonNum}`;
 
-    return n1 + n2;
+const get3Pokemon = async (n1, n2, n3) => {
+    const pokemonPromise1 = fetch(getPokemon(n1)).then(res => res.json());
+    const pokemonPromise2 = fetch(getPokemon(n2)).then(res => res.json());
+    const pokemonPromise3 = fetch(getPokemon(n3)).then(res => res.json());
+
+    const pokemons = await Promise.all([pokemonPromise1, pokemonPromise2, pokemonPromise3]);
+
+    pokemons.map(pokemon => console.log(pokemon.name));
 };
-console.log(aSum(1, 'a'));
-aSum(1, 'a')
-    .then(aSumResolveReturn => console.log(aSumResolveReturn))
-    .catch(aSumRejectReturn => console.log('ErrOr:', aSumRejectReturn));
+get3Pokemon(2, 5, 9);
